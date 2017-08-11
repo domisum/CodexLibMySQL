46896d2a10ec0677d42498602fbb17cf6471e37b
Dominik <dominik.weissenseel@gmx.de>
2017-08-11 20:34:16 +0200

    Initial commit

---
+++ src/main/java/de/domisum/lib/codex/mysql/MySQLConPoolWrapper.java
@@ -0,0 +1,50 @@
+package de.domisum.lib.codex.mysql;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import de.domisum.lib.auxilium.util.java.annotations.APIUsage;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@APIUsage
+public class MySQLConPoolWrapper
+{
+
+	private HikariDataSource connectionPool;
+
+
+	// INIT
+	@APIUsage public MySQLConPoolWrapper(String serverAddress, int serverPort, String username, String password)
+	{
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDataSourceClassName("com.mysql.cj.jdbc.MysqlDataSource");
+
+		hikariConfig.addDataSourceProperty("serverName", serverAddress);
+		hikariConfig.addDataSourceProperty("port", serverPort);
+		hikariConfig.addDataSourceProperty("user", username);
+		hikariConfig.addDataSourceProperty("password", password);
+
+		hikariConfig.setMaximumPoolSize(3);
+
+
+		this.connectionPool = new HikariDataSource(hikariConfig);
+	}
+
+
+	// GETTERS
+	@APIUsage public Connection getConnection()
+	{
+		try
+		{
+			return this.connectionPool.getConnection();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+
+		throw new IllegalStateException("Hikari didn't return a connection");
+	}
+
+}
